ddc65f946e159fca60efa47760d0f827324ec114
Hassnain0007 <hassnainalibhatti324@gmail.com>
2024-05-24 11:01:16 +0500

    Add files via upload

---
+++ 12 Greetings.ts
@@ -0,0 +1,5 @@
+let names :string[] = [ 'Hasnain','Bilal','Saqulain','Imran','Fareed','Zain','Mehdi'] ;
+const message :string = 'Welcome to Typescript class: '
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
